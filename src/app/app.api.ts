146ae4e3972658132a526c4f57197bddef7fb561
VieriElCrack1 <santosvieri9@gmail.com>
2024-08-19 19:35:35 -0500

    iniciando proyecto sesion 08 - front end

---
+++ src/app/app.api.ts
@@ -0,0 +1,3 @@
+export class AppApi {
+    public static API = "http://localhost:8090/rest";
+}
---
+++ src/app/components/crud-docente/crud-docente.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crud-docente',
+  standalone: true,
+  imports: [],
+  templateUrl: './crud-docente.component.html',
+  styleUrl: './crud-docente.component.css'
+})
+export class CrudDocenteComponent {
+
+  constructor() {}
+
+  
+}
